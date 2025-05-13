058f8472e0067fb55fc3f62e4d170495ade6d66c
JuneTech <junetech0@gmail.com>
2025-05-13 13:04:36 +0900

    update according to routix 0.0.3

--- hybridflowshop/hfs_cp_lns.py
+++ hybridflowshop/hfs_cp_lns.py
@@ -56,7 +56,7 @@ class HybridFlowShopCpLnsController(SubroutineController):
 
     def draw_incumbent_gantt(self, output_path: Path | None = None) -> None:
         if output_path is None:
-            output_path = self.get_file_path_by_for_subroutine("_gantt.png")
+            output_path = self.get_file_path_for_subroutine("_gantt.png")
         self.incumbent_solution_manager.save_gantt_as_png(output_path)
 
     def update_incumbent_solution(self) -> None:
