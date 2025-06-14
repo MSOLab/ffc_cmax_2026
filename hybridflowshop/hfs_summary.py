22ba5d39bc256e97cf618a96bd4b44fbe67b50ed
JuneTech <junetech0@gmail.com>
2025-06-14 22:06:27 +0900

    add optional encoding argument to HFSSummary.save method

--- hybridflowshop/hfs_summary.py
+++ hybridflowshop/hfs_summary.py
@@ -27,15 +27,21 @@ class HFSSummary:
         outputs_value_str = ",".join(str(value) for value in outputs_values)
         return f"\n{inputs_value_str},{outputs_value_str}"
 
-    def save(self, output_path: Path):
+    def save(self, output_path: Path, encoding: str = "utf-8"):
+        """Save the summary to a file in comma-separated values format.
+
+        Args:
+            output_path (Path): The path to the output file where the summary will be saved.
+            encoding (str, optional): The encoding to use when saving the file. Defaults to "utf-8".
+        """
         # make sure the directory exists
         output_path.parent.mkdir(parents=True, exist_ok=True)
         # write data
         # if the file already exists, append to the file
         if output_path.exists():
-            with open(output_path, "a") as f:
+            with open(output_path, "a", encoding=encoding) as f:
                 f.write(self.comma_seperated_values())
         else:
-            with open(output_path, "w") as f:
+            with open(output_path, "w", encoding=encoding) as f:
                 f.write(self.comma_separated_values_header())
                 f.write(self.comma_seperated_values())
