c5b5b332d13f36db454634dbece7500ab02c7bae
JuneTech <junetech0@gmail.com>
2025-09-08 17:09:54 +0900

    Refactor precision checks in objective value comparisons for better clarity

--- hybridflowshop/solution_manager.py
+++ hybridflowshop/solution_manager.py
@@ -2,6 +2,8 @@
 Defines the concrete implementation of the solution manager for the Hybrid Flowshop Scheduling problem.
 """
 
+import math
+
 from routix.solution_manager import SolutionManager
 
 from .report import HfsSubroutineReport
@@ -26,11 +28,17 @@ class HfsSolutionManager(SolutionManager[HfsSubroutineReport, HybridFlowshopSche
     def _a_is_better_obj_value(self, value_a: float, value_b: float | None) -> bool:
         if value_b is None:
             return True
+        # False if close enough (considering floating point precision)
+        if math.isclose(value_a, value_b, rel_tol=1e-9, abs_tol=1e-12):
+            return False
         # A smaller makespan is better (minimization).
         return value_a < value_b
 
     def _a_is_better_obj_bound(self, bound_a: float, bound_b: float | None) -> bool:
         if bound_b is None:
             return True
+        # False if close enough (considering floating point precision)
+        if math.isclose(bound_a, bound_b, rel_tol=1e-9, abs_tol=1e-12):
+            return False
         # For a minimization problem, a higher lower bound is better.
         return bound_a > bound_b
