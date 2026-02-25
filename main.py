bff1666fd96d1a2a651f48f1f76437baeb5cf674
JuneTech <junetech0@gmail.com>
2026-02-25 18:39:21 +0900

    Remove yaml.UnsafeLoader usage
    - use load_yaml and dump_yaml from routix.io.yaml

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ from routix import (
     StoppingCriteria,
     SubroutineFlowValidator,
 )
-from routix.io import init_timestamped_working_dir, object_to_yaml
+from routix.io.path import init_timestamped_working_dir
+from routix.io.yaml import dump_yaml
 from routix.type_defs import RunMode
 from schore.parameters_examples.parallel_shop.identical_flow import (
     HybridFlowshopParameters,
@@ -64,8 +65,8 @@ def run_experiment(
         )
         # if run_mode is RunMode.FULL_RUN:
         if run_mode in {RunMode.FULL_RUN, RunMode.RESUME}:
-            object_to_yaml(config.to_dict(), main_metadata_dump_path)
-            object_to_yaml(pra_common_params_dict, pra_common_params_dump_path)
+            dump_yaml(config.to_dict(), main_metadata_dump_path)
+            dump_yaml(pra_common_params_dict, pra_common_params_dump_path)
         elif run_mode is RunMode.POST_PROCESS_ONLY:
             if not main_metadata_dump_path.is_file():
                 raise FileNotFoundError(
