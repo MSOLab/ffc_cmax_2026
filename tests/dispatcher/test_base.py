124df582baf7bc6fd969171a86778039c8f97c19
JuneTech <junetech0@gmail.com>
2026-02-25 15:11:43 +0900

    fix: remove outdated Dispatcher import from tests
    - Replace Dispatcher import with BN2DDispatcher in test_base.py
    - Update TestDispatcherStructure to test BN2DDispatcher instead

--- tests/dispatcher/test_base.py
+++ tests/dispatcher/test_base.py
@@ -2,7 +2,7 @@
 
 from hybridflowshop.dispatcher import (
     BaseDispatcher,
-    Dispatcher,
+    BN2DDispatcher,
     JobDispatcher,
     MixedDispatcher,
     StageDispatcher,
@@ -33,14 +33,6 @@ class TestBaseDispatcher:
 class TestDispatcherStructure:
     """Tests for dispatcher class hierarchy."""
 
-    def test_dispatcher_extends_mixed_dispatcher(self) -> None:
-        """Test Dispatcher extends MixedDispatcher."""
-        assert issubclass(Dispatcher, MixedDispatcher)
-
-    def test_mixed_dispatcher_extends_base_dispatcher(self) -> None:
-        """Test MixedDispatcher extends BaseDispatcher."""
-        assert issubclass(MixedDispatcher, BaseDispatcher)
-
     def test_stage_dispatcher_extends_base_dispatcher(self) -> None:
         """Test StageDispatcher extends BaseDispatcher."""
         assert issubclass(StageDispatcher, BaseDispatcher)
@@ -48,3 +40,11 @@ class TestDispatcherStructure:
     def test_job_dispatcher_extends_base_dispatcher(self) -> None:
         """Test JobDispatcher extends BaseDispatcher."""
         assert issubclass(JobDispatcher, BaseDispatcher)
+
+    def test_mixed_dispatcher_extends_base_dispatcher(self) -> None:
+        """Test MixedDispatcher extends BaseDispatcher."""
+        assert issubclass(MixedDispatcher, BaseDispatcher)
+
+    def test_bn2d_dispatcher_extends_base_dispatcher(self) -> None:
+        """Test BN2DDispatcher extends BaseDispatcher."""
+        assert issubclass(BN2DDispatcher, BaseDispatcher)
