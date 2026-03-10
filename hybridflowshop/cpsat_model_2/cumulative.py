ec6dbfd33b288b0a1da3cdcc44e06527c0276914
JuneTech <junetech0@gmail.com>
2026-03-11 03:57:54 +0900

    20260311T035712_760779 run setting
    Computer: mso2
    
    - Uncomment _add_job_completion_link_constraints in both
      build_model and build_model_2 methods
    - Update main_metadata.yaml scenario output directory and
      description for 20260310-16

--- hybridflowshop/cpsat_model_2/cumulative.py
+++ hybridflowshop/cpsat_model_2/cumulative.py
@@ -51,7 +51,7 @@ class BaseModelBuilder:
             mdl, params, horizon, tighten_ranges=True
         )
         self._add_structural_constraints(mdl, params, variables)
-        # self._add_job_completion_link_constraints(mdl, params, variables)
+        self._add_job_completion_link_constraints(mdl, params, variables)
         # self._add_inter_stage_structural_constraints(mdl, params, variables)
         self._define_objective(
             mdl,
@@ -76,7 +76,7 @@ class BaseModelBuilder:
             mdl, params, stage_2_mc_2_horizon
         )
         self._add_structural_constraints(mdl, params, variables, stage_2_mc_2_horizon)
-        # self._add_job_completion_link_constraints(mdl, params, variables)
+        self._add_job_completion_link_constraints(mdl, params, variables)
         # self._add_inter_stage_structural_constraints(mdl, params, variables)
         self._define_objective(mdl, params, variables)
         mdl.set_num_base_constraints()
