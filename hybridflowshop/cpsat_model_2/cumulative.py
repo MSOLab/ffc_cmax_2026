c56393a1124a0f3a005ae15c6a314b871e31fe52
JuneTech <junetech0@gmail.com>
2026-03-11 02:24:39 +0900

    Add subroutine flow config and enable tighten_ranges
    20260311T022416_283624 run setting on mso2
    
    - Add new subroutine flow config (subroutine_flow_20260310-15.yaml)
      with initialize_by_best_of_selected_dispatches and neh_cp methods
    - Enable tighten_ranges=True in CumulativeVars creation for CP-SAT model
    - Update main_metadata.yaml to add new scenario (20260310-15) and
      renumber 20260310-13 to 20260310-14

--- hybridflowshop/cpsat_model_2/cumulative.py
+++ hybridflowshop/cpsat_model_2/cumulative.py
@@ -47,7 +47,9 @@ class BaseModelBuilder:
     ) -> tuple[CustomCpModel, Params, CumulativeVars]:
         mdl = CustomCpModel()
         params: Params = self._make_params(instance)
-        variables: CumulativeVars = self._make_vars(mdl, params, horizon)
+        variables: CumulativeVars = self._make_vars(
+            mdl, params, horizon, tighten_ranges=True
+        )
         self._add_structural_constraints(mdl, params, variables)
         # self._add_job_completion_link_constraints(mdl, params, variables)
         # self._add_inter_stage_structural_constraints(mdl, params, variables)
